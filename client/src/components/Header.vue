4d044581db4c49f34636bab0a143bf004c755e26
KHERRAZ Mohammed Achraf <achraf.sena@gmail.com>
2024-05-22 15:50:57 +0200

    side_bar init

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -5,26 +5,49 @@
       <h1>Astroverse</h1>
     </div>
     <NavBar />
-    <h2>LOGIN</h2>
+    <button :class="['login', { active: loginClicked }]" @click="toggleLogin">
+      <span class="material-symbols-outlined">{{ logIconChange }}</span>
+    </button>
+    <SideBar :showSideBar="loginClicked">
+      <div class="log-btn-container" v-if="loginClicked">
+        <RouterLink to="/login" class="log-btn">Login</RouterLink>
+        <RouterLink to="/register" class="log-btn">Register</RouterLink>
+      </div>
+      <!-- here add le panier -->
+    </SideBar>
   </header>
 </template>
 
 <script setup>
 import NavBar from "./NavBar.vue";
-import { ref } from "vue";
+import SideBar from "../ui/SideBar.vue";
+import { computed, ref } from "vue";
 import logoPath from "../assets/images/logo.png"; // Importer le logo
 
 const logo = ref(logoPath); // Référence au chemin du logo
+const loginClicked = ref(false);
+
+const logIconChange = computed(() => {
+  return loginClicked.value ? "close" : "account_circle";
+});
+
+const toggleLogin = () => {
+  loginClicked.value = !loginClicked.value;
+};
 </script>
 
 <style lang="scss" scoped>
 header {
+  position: fixed;
+  top: 0;
+  width: 100%;
   font-family: "Nippo", sans-serif;
   background-color: white;
   display: flex;
   justify-content: space-around;
   align-items: center;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  height: 100px;
   .logo {
     display: flex;
     align-items: center;
@@ -35,6 +58,53 @@ header {
     img {
       width: 100px;
       height: 100px;
+    }
+  }
+  .login {
+    position: relative;
+    cursor: pointer;
+    color: black;
+    background-color: white;
+    transition: all 0.3s ease;
+    padding: 10px;
+    border-radius: 100%;
+    border: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &:hover,
+    &.active {
+      color: white;
+      background-color: #f2a45a;
+    }
+    &:hover {
+      transform: scale(1.1);
+    }
+    .material-symbols-outlined {
+      font-size: 30px;
+      font-variation-settings: "FILL" 1, "wght" 400, "GRAD" 0, "opsz" 48;
+      transition: transform 0.5s ease;
+    }
+    &.active .material-symbols-outlined {
+      transform: rotate(180deg);
+    }
+  }
+  .log-btn-container {
+    background-color: white;
+    padding: 100px;
+    border-radius: 15px;
+box-shadow: rgba(0, 0, 0, 0.56) 0px 22px 70px 4px;    .log-btn {
+      display: block;
+      padding: 10px;
+      text-align: center;
+      text-decoration: none;
+      color: black;
+      font-weight: bold;
+      transition: all 0.3s ease;
+      &:hover {
+        color: white;
+        background-color: #f2a45a;
+      }
     }
   }
 }
---
+++ client/src/ui/SideBar.vue
@@ -0,0 +1,49 @@
+<!-- src/components/NavBar.vue -->
+<template>
+  <Transition name="slide">
+  <div v-if="showSideBar" class="side-bar">
+    <slot>
+      <p>Contenu par défaut si aucun contenu n'est passé.</p>
+    </slot>
+  </div>
+</Transition>
+</template>
+<script setup>
+import { ref } from "vue";
+
+
+// Watch the parent prop to sync with the ref
+const props = defineProps({
+  showSideBar: {
+    type: Boolean,
+    default: false,
+  }
+});
+
+
+
+</script>
+
+<style lang="scss" scoped>
+.side-bar {
+  position: fixed;
+  top: 100px;
+  right: 0;
+  width: 20%;
+  height: 100%;
+  background-color: #f2a45a;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  z-index: 1000;
+  padding-top: 20px;
+}
+.slide-enter-active, .slide-leave-active {
+  transition: transform 0.5s ease;
+}
+
+.slide-enter-from, .slide-leave-to {
+  transform: translateX(100%);
+}
+</style>
