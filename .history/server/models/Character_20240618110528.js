1b27d9fd907d92efe918d2b43382cdd99797d94c
Youri Ghlis <ghlisyouri@gmail.com>
2024-06-18 11:25:12 +0200

    Yahouuuuuuu

---
+++ .history/server/models/Character_20240618110528.js
@@ -0,0 +1,33 @@
+// models/Character.js
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
+import Universe from './Universe.js';
+
+const Character = sequelize.define('Character', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true  // Ajoute une contrainte d'unicité
+  },
+  universe: {
+    type: DataTypes.UUID,
+    references: {
+      model: Universe,
+      key: 'id',
+    },
+    allowNull: false
+  }
+}, {
+  timestamps: false,
+  tableName: 'characters'
+});
+
+Universe.hasMany(Character, { foreignKey: 'universe' });
+Character.belongsTo(Universe, { foreignKey: 'universe' });
+
+export default Character;
