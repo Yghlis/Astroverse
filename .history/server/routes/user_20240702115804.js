8a19221f1a16cb837d82c1386e12056ad3547c99
Youri Ghlis <ghlisyouri@gmail.com>
2024-07-02 19:58:50 +0200

    Filters fix and secure routes

---
+++ .history/server/routes/user_20240702115804.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { getUsers, getUserById, addUser, addUserFromBackoffice, updateUser, deleteUser } from '../controllers/user.js';
+import { authenticateToken, requireRole } from '../middleware/auth.js';
+
+const router = express.Router();
+
+// Obtenir tous les utilisateurs (accessible uniquement aux administrateurs)
+router.get('/', authenticateToken, requireRole('ROLE_ADMIN'), getUsers);
+
+// Obtenir un utilisateur par ID (accessible uniquement à l'utilisateur authentifié ou aux administrateurs)
+router.get('/:id', authenticateToken, getUserById);
+
+// Créer un nouvel utilisateur depuis le site (accessible à tous)
+router.post('/', addUser);
+
+// Créer un nouvel utilisateur depuis le backoffice (accessible uniquement aux administrateurs)
+router.post('/admin', authenticateToken, requireRole('ROLE_ADMIN'), addUserFromBackoffice);
+router.put('/:id', authenticateToken, requireRole('ROLE_ADMIN'), updateUser);
+router.delete('/:id', authenticateToken, requireRole('ROLE_ADMIN'), deleteUser);
+
+export default router;
