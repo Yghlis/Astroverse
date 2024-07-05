dcd0ffaadc028ae1c26cf200ac98564112260df1
Youri Ghlis <ghlisyouri@gmail.com>
2024-07-05 15:43:43 +0200

    Add alert for change password after 60 days

---
+++ .history/server/routes/user_20240705125905.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { getUsers, getUserById, addUser, addUserFromBackoffice, updateUser, deleteUser } from '../controllers/user.js';
+import { authenticateToken, requireRole } from '../middleware/auth.js';
+
+const router = express.Router();
+router.get('/', authenticateToken, requireRole('ROLE_ADMIN'), getUsers);
+router.get('/:id', authenticateToken, getUserById);
+router.post('/', addUser);
+router.post('/admin', authenticateToken, requireRole('ROLE_ADMIN'), addUserFromBackoffice);
+router.put('/:id', authenticateToken, requireRole('ROLE_ADMIN'), updateUser);
+router.delete('/:id', authenticateToken, requireRole('ROLE_ADMIN'), deleteUser);
+router.put('/:id/password-reminder-reset', authenticateToken, resetPasswordChangeReminder);
+
+export default router;
