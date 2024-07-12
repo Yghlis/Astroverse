995d11796889fe90c40a89a8be6f38c58d543c8d
KHERRAZ Mohammed Achraf <achraf.sena@gmail.com>
2024-07-12 03:06:04 +0200

    design register done

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,114 +1,257 @@
 <template>
-  <div>
-    <h2>Register</h2>
+  <div class="register-container">
+    <h2>Créez votre compte</h2>
     <form @submit.prevent="register">
-      <div>
-        <label for="first_name">First Name:</label>
-        <input type="text" id="first_name" v-model="first_name" required />
+      <div class="form-group">
+        <label for="first_name">Prénom<span>*</span>:</label>
+        <input
+          type="text"
+          id="first_name"
+          v-model="first_name"
+          placeholder="Entrez votre prénom"
+          required
+        />
       </div>
-      <div>
-        <label for="last_name">Last Name:</label>
-        <input type="text" id="last_name" v-model="last_name" required />
+      <div class="form-group">
+        <label for="last_name">Nom<span>*</span>:</label>
+        <input
+          type="text"
+          id="last_name"
+          v-model="last_name"
+          placeholder="Entrez votre nom"
+          required
+        />
       </div>
-      <div>
-        <label for="email">Email:</label>
-        <input type="email" id="email" v-model="email" required />
+      <div class="form-group">
+        <label for="email">Email<span>*</span>:</label>
+        <input
+          type="email"
+          id="email"
+          v-model="email"
+          placeholder="Entrez votre email"
+          required
+        />
       </div>
-      <div>
-        <label for="password">Password:</label>
-        <input type="password" id="password" v-model="password" @input="evaluatePassword" required />
-        <div :style="{ color: passwordStrength.color }">{{ passwordStrength.message }}</div>
+      <div class="form-group">
+        <label for="password">Mot de passe<span>*</span>:</label>
+        <input
+          type="password"
+          id="password"
+          v-model="password"
+          placeholder="Créez un mot de passe"
+          @input="evaluatePassword"
+          required
+        />
+        <div :style="{ color: passwordStrength.color }">
+          {{ passwordStrength.message }}
+        </div>
       </div>
-      <div>
-        <label for="confirmPassword">Confirm Password:</label>
-        <input type="password" id="confirmPassword" v-model="confirmPassword" required />
+      <div class="form-group">
+        <label for="confirmPassword"
+          >Confirmez le mot de passe<span>*</span>:</label
+        >
+        <input
+          type="password"
+          id="confirmPassword"
+          v-model="confirmPassword"
+          placeholder="Confirmez votre mot de passe"
+          required
+        />
       </div>
-      <button type="submit">Register</button>
+      <button type="submit">Inscription</button>
     </form>
+    <p>
+      Avez vous déjà un compte ?
+      <button class="login" @click="goToLogin">Connectez-vous</button>
+    </p>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      first_name: '',
-      last_name: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
-      passwordStrength: {
-        message: '',
-        color: ''
-      }
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { useSidebarStore } from "../stores/sidebarStore";
+const sidebarStore = useSidebarStore();
+
+const first_name = ref("");
+const last_name = ref("");
+const email = ref("");
+const password = ref("");
+const confirmPassword = ref("");
+const passwordStrength = ref({
+  message: "",
+  color: "",
+});
+const router = useRouter();
+
+const evaluatePassword = () => {
+  const hasLowercase = /[a-z]/.test(password.value);
+  const hasUppercase = /[A-Z]/.test(password.value);
+  const hasNumber = /[0-9]/.test(password.value);
+  const hasSpecialChar = /[\W_]/.test(password.value);
+  const isLongEnough = password.value.length >= 12;
+
+  if (
+    isLongEnough &&
+    hasLowercase &&
+    hasUppercase &&
+    hasNumber &&
+    hasSpecialChar
+  ) {
+    passwordStrength.value.message = "Mot de passe solide";
+    passwordStrength.value.color = "green";
+  } else {
+    passwordStrength.value.message =
+      "Mot de passe trop faible. Il doit contenir au moins 12 caractères, avec au moins un symbole, un chiffre, une lettre minuscule et une lettre majuscule.";
+    passwordStrength.value.color = "red";
+  }
+};
+
+const register = async () => {
+  const hasLowercase = /[a-z]/.test(password.value);
+  const hasUppercase = /[A-Z]/.test(password.value);
+  const hasNumber = /[0-9]/.test(password.value);
+  const hasSpecialChar = /[\W_]/.test(password.value);
+  const isLongEnough = password.value.length >= 12;
+
+  if (
+    !(
+      isLongEnough &&
+      hasLowercase &&
+      hasUppercase &&
+      hasNumber &&
+      hasSpecialChar
+    )
+  ) {
+    alert(
+      "Le mot de passe est trop faible. Il doit contenir au moins 12 caractères, avec au moins un symbole, un chiffre, une lettre minuscule et une lettre majuscule."
+    );
+    return;
+  }
+
+  if (password.value !== confirmPassword.value) {
+    alert("Les mots de passe ne correspondent pas");
+    return;
+  }
+
+  try {
+    const response = await fetch("http://localhost:8000/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        first_name: first_name.value,
+        last_name: last_name.value,
+        email: email.value,
+        password: password.value,
+        confirmPassword: confirmPassword.value,
+      }),
+    });
+    const data = await response.json();
+    console.log("Register response:", data);
+    if (response.ok) {
+      router.push("/");
+    } else {
+      alert(data.message || "Registration failed");
     }
-  },
-  methods: {
-    evaluatePassword() {
-      const hasLowercase = /[a-z]/.test(this.password);
-      const hasUppercase = /[A-Z]/.test(this.password);
-      const hasNumber = /[0-9]/.test(this.password);
-      const hasSpecialChar = /[\W_]/.test(this.password);
-      const isLongEnough = this.password.length >= 12;
+  } catch (error) {
+    console.error("Error during registration:", error);
+    alert("An error occurred during registration");
+  }
+};
 
-      if (isLongEnough && hasLowercase && hasUppercase && hasNumber && hasSpecialChar) {
-        this.passwordStrength.message = 'Mot de passe solide';
-        this.passwordStrength.color = 'green';
-      } else {
-        this.passwordStrength.message = 'Mot de passe trop faible. Il doit contenir au moins 12 caractères, avec au moins un symbole, un chiffre, une lettre minuscule et une lettre majuscule.';
-        this.passwordStrength.color = 'red';
-      }
-    },
-    async register() {
-      // Vérifier si le mot de passe est solide
-      const hasLowercase = /[a-z]/.test(this.password);
-      const hasUppercase = /[A-Z]/.test(this.password);
-      const hasNumber = /[0-9]/.test(this.password);
-      const hasSpecialChar = /[\W_]/.test(this.password);
-      const isLongEnough = this.password.length >= 12;
-
-      if (!(isLongEnough && hasLowercase && hasUppercase && hasNumber && hasSpecialChar)) {
-        alert("Le mot de passe est trop faible. Il doit contenir au moins 12 caractères, avec au moins un symbole, un chiffre, une lettre minuscule et une lettre majuscule.");
-        return;
-      }
-
-      if (this.password !== this.confirmPassword) {
-        alert("Les mots de passe ne correspondent pas");
-        return;
-      }
-
-      try {
-        const response = await fetch('http://localhost:8000/auth/signup', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            first_name: this.first_name,
-            last_name: this.last_name,
-            email: this.email,
-            password: this.password,
-            confirmPassword: this.confirmPassword
-          }),
-        });
-        const data = await response.json();
-        console.log('Register response:', data);
-        if (response.ok) {
-          this.$router.push('/');
-        } else {
-          alert(data.message || 'Registration failed');
-        }
-      } catch (error) {
-        console.error('Error during registration:', error);
-        alert('An error occurred during registration');
-      }
-    },
-  },
-}
+const goToLogin = () => {
+  sidebarStore.toggleUserSidebar();
+};
 </script>
 
-<style type="scss" scoped>
-div {
-  margin-top: 100px;
+<style lang="scss" scoped>
+.register-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  h2 {
+    margin-bottom: 20px;
+    font-size: 54px;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    min-width: 300px;
+    width: 500px;
+    border: 1px solid #ccc;
+    padding: 25px;
+    border-radius: 25px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    .form-group {
+      margin-bottom: 15px;
+      div {
+        margin-top: 5px;
+      }
+      label {
+        display: block;
+        margin-bottom: 10px;
+        font-weight: bold;
+        font-size: 20px;
+      }
+      input {
+        width: 100%;
+        padding: 15px;
+        font-size: 20px;
+        border: 1px solid #ccc;
+        border-radius: 25px;
+        transition: all 0.3s ease;
+        &::placeholder {
+          color: #ccc;
+        }
+        &:focus {
+          outline: none;
+          border-color: #8b8b8b;
+        }
+      }
+      span {
+        color: red;
+      }
+    }
+    button {
+      margin-top: 30px;
+      padding: 15px;
+      border: none;
+      border-radius: 25px;
+      background-color: black;
+      color: white;
+      font-size: 20px;
+      font-weight: bold;
+      cursor: pointer;
+      transition: all 0.3s ease;
+      &:hover {
+        background-color: #333;
+      }
+      &:focus {
+        outline: none;
+        background-color: #9b9b9b;
+      }
+    }
+  }
+  p {
+    margin-top: 20px;
+    font-size: 16px;
+    .login {
+      background-color: white;
+      color: #f2a45a;
+      margin-top: 0;
+      margin-left: 3px;
+      font-size: 16px;
+      padding: 0;
+      border: none;
+      cursor: pointer;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
 }
 </style>
