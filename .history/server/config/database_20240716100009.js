299150eb474e94802dbf459d091ca39fe4604799
Youri Ghlis <ghlisyouri@gmail.com>
2024-07-16 10:07:46 +0200

    jen peux plus

--- .history/server/config/database_20240716100009.js
+++ .history/server/config/database_20240716100009.js
@@ -3,13 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'postgres',
   logging: false,
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
+      rejectUnauthorized: false // Vous pouvez ajuster cette option selon vos besoins
     }
   }
 });
