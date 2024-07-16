c0a732e4970afe5ce22845dad39ee02205d50e32
Youri Ghlis <ghlisyouri@gmail.com>
2024-07-16 09:27:02 +0200

    Add all fix for deploiement backend

--- .history/server/config/database_20240716091515.js
+++ .history/server/config/database_20240716091515.js
@@ -1,8 +1,18 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Vous pouvez ajuster cette option selon vos besoins
+    }
+  }
 });
 
 export default sequelize;
